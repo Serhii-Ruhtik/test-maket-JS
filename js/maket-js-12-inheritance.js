2cde9b63e1dffb49ab64dde457e1528da33e93d9
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-04-19 18:45:16 +0300

    12

--- js/maket-js-12-inheritance.js
+++ js/maket-js-12-inheritance.js
@@ -4,42 +4,83 @@
  *  - super()
  */
 class Hero {
-    constructor(name = "hero", xp = 0) {
+    constructor({ name = 'hero', xp = 0 }) {
         this.name = name;
         this.xp = xp;
+    }
+    gainXp(amount) {
+        console.log(`${this.name} отримує ${amount} досвіду.`);
+        this.xp += amount;
+    }
+    attack() {
+        console.log(`${this.name} атакує використовуючи ${this.weapon}`);
+    }
 }
-gainXp(amount){
-    console.log(`${this.name} отримує ${amount} досвіду.`)
-    this.xp += amount;
-}
-}
-
-// const mango = new Hero({
-//     name: "mango",
-//     xp: 1000
-// })
-
-// console.log(mango);
-// mango.gainXp(1000);
-// console.log(mango);
 
 class Warrior extends Hero {
-    constructor(name, xp, weapon,) {
-        super(name, xp);
+    constructor({ weapon, ...restProps } = {}) {
+        super(restProps);
         this.weapon = weapon;
     }
 }
 
-const lemon = new Warrior('lemon', 1250, 'knife')
+class Berseck extends Warrior {
+    constructor({ warcry, ...restProps } = {}) {
+        super(restProps);
 
-console.log(lemon);
-lemon.gainXp(1000);
-console.log(lemon);
+        this.warcry = warcry;
+    }
+
+    babyRage() {
+        console.log(this.warcry);
+    }
+}
+
+const ajax = new Berseck({
+    name: 'ajax',
+    xp: 500,
+    weapon: 'axe',
+    warcry: 'waaaaaah',
+});
+console.log(ajax);
+
+ajax.babyRage();
+ajax.attack();
+ajax.gainXp(525);
+
+console.log(ajax);
 
 
-console.log('Warrior.prototype', Warrior.prototype);
-console.log('Hero.prototype', Hero.prototype);
+class Mage extends Hero {
+    constructor({ spells, ...restProps } = {}) {
+        super(restProps);
 
+        this.spells = spells;
+    }
+
+    cast() {
+        console.log(`${this.name} використовує магію`);
+    }
+}
+
+// const mango = new Warrior({ name: 'mango', xp: 1000, weapon: 'knife' })
+
+// console.log(mango);
+// mango.attack();
+// mango.gainXp(1000);
+// console.log(mango);
+
+// const poly = new Mage ({ name: 'poly', xp: 500, spells: ['incantation'] });
+
+// console.log(poly);
+// poly.cast();
+// poly.gainXp(1000);
+// console.log(poly);
+
+// console.log('Warrior.prototype', Warrior.prototype);
+// console.log('Hero.prototype', Hero.prototype);
+
+// !============================================================
 // console.log(
 //     'mango.__proto__ === Warrior.prototype ',
 //     mango.__proto__ === Warrior.prototype,
